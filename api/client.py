5b572ff5c3feb830dcfabb3b0ff2c4775f3b116f
Yukun-Mac <daiyukun2013@gmail.com>
2025-05-19 23:19:45 +0200

    Finish the client setup function

--- api/client.py
+++ api/client.py
@@ -24,11 +24,16 @@ class MCPClient:
             args=[server_script_path],
             env=None
         )
-        stdio_transport = await self.exit_stack.enter_async_context(stdio_client(server_params))
+        stdio_transport = await self.exit_stack.enter_async_context(stdio_client(server_parameters))
         self.stdio, self.write = stdio_transport
         self.session = await self.exit_stack.enter_async_context(ClientSession(self.stdio, self.write))
 
         await self.session.initialize()
+
+        # list available tools
+        response = await self.session.list_tools()
+        tools = response.tools
+        print("\nConnected to server with tools:", [tool.name for tool in tools])
     # Call tools from the MCP server
     # get MCP tool
     # process a query
